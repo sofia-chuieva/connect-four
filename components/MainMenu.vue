8c6142949ff591e68e09c2bd3246e569167481a2
sschu <sschuyeva@gmail.com>
2025-02-09 21:05:37 +0000

    amend styling

--- components/MainMenu.vue
+++ components/MainMenu.vue
@@ -32,60 +32,60 @@
   </div>
 </template>
 
-<style>
+<style scoped>
 @reference "../assets/css/main.css";
 
 .container {
-  @apply mx-auto max-w-3xl w-full h-screen flex items-center justify-center;
-}
+  @apply mx-auto max-w-3xl px-6 w-full h-screen flex items-center justify-center;
 
-.wrapper {
-  @apply relative;
-}
+  .wrapper {
+    @apply relative;
 
-.menu {
-  @apply relative z-20 min-w-[30rem] gap-16 p-10 items-center justify-between flex flex-col bg-dark-blue rounded-4xl border-[3px] border-black;
-}
+    .menu {
+      @apply relative z-20 md:min-w-[30rem] gap-16 p-10 items-center justify-between flex flex-col bg-dark-blue rounded-4xl border-[3px] border-black;
 
-.buttons {
-  @apply flex flex-col w-full gap-8;
-}
+      .buttons {
+        @apply flex flex-col w-full gap-8;
 
-button {
-  @apply flex cursor-pointer px-8 py-3 relative z-20 justify-between items-center uppercase rounded-3xl border-[3px] border-black;
-}
+        button {
+          @apply flex cursor-pointer px-8 py-3 relative z-20 justify-between items-center uppercase rounded-3xl border-[3px] border-black;
+        }
 
-.player-vs-cpu-btn {
-  @apply bg-red;
-}
+        .player-vs-cpu-btn {
+          @apply bg-red;
+        }
 
-.player-vs-cpu-bg {
-  @apply w-[395px] h-[74px] top-[36%];
-}
+        .player-vs-cpu-bg {
+          @apply w-[395px] h-[74px] top-[36%];
+        }
 
-.player-vs-player-btn {
-  @apply bg-orange;
-}
+        .player-vs-player-btn {
+          @apply bg-orange;
+        }
 
-.player-vs-player-bg {
-  @apply w-[395px] h-[74px] top-[58%];
-}
+        .player-vs-player-bg {
+          @apply w-[395px] h-[74px] top-[58%];
+        }
 
-.player-vs-player-bg,
-.player-vs-cpu-bg,
-.rules-bg {
-  @apply absolute bg-black rounded-3xl z-10;
-}
+        .player-vs-player-bg,
+        .player-vs-cpu-bg,
+        .rules-bg {
+          @apply absolute bg-black rounded-3xl z-10;
+        }
 
-.rules-btn {
-  @apply bg-white py-5;
-}
+        .rules-btn {
+          @apply bg-white py-5;
+        }
 
-.rules-bg {
-  @apply w-[395px] h-[74px] top-[79%];
-}
+        .rules-bg {
+          @apply w-[395px] h-[74px] top-[79%];
+        }
+      }
+    }
 
-.menu-bg {
-  @apply absolute z-10 h-[450px] w-[30rem] bg-black rounded-4xl top-6;
+    .menu-bg {
+      @apply absolute z-10 h-full w-full bg-black rounded-4xl top-6;
+    }
+  }
 }
 </style>
