f913dd0c6c83487a47a8ab7f8dd9843dede3e031
Sofia Chuieva <schuieva@themission.co.uk>
2025-02-27 18:26:26 +0000

    add header

--- components/GameBoard.vue
+++ components/GameBoard.vue
@@ -1,46 +1,47 @@
 <template>
   <div class="container">
-    <div class="player-wrapper">
-      <div class="player player-1">
-        <img src="../assets/images/player-one-smiley-face.svg" alt="" />
-        <p class="uppercase">Player 1</p>
-        <h1 class="pt-4 pb-10">5</h1>
-      </div>
-      <div class="player-bg"></div>
-    </div>
-    <div class="board-wrapper">
-      <Board class="board-background" />
-      <svg class="board-overlay">
-        <g v-for="(row, rowIndex) in board" :key="'row-' + rowIndex">
-          <rect
-            v-for="(_, colIndex) in row"
-            :key="rowIndex + '-' + colIndex"
-            :x="colIndex * cellWidth"
-            :y="rowIndex * cellHeight"
-            :width="cellWidth"
-            :height="cellHeight"
-            fill="transparent"
-            stroke="rgba(255, 0, 0, 0.5)"
-            stroke-width="2"
-            :id="rowIndex + '-' + colIndex"
-          />
-        </g>
-      </svg>
-      <div class="timer">
-        <div class="timer-wrapper">
-          <p class="font-bold text-white text-base">PLAYER 1’S TURN</p>
-          <h1 class="text-white">10s</h1>
+    <Header />
+    <div class="game-board">
+      <div class="player-wrapper">
+        <div class="player player-1">
+          <img src="../assets/images/player-one-smiley-face.svg" alt="" />
+          <p class="uppercase">Player 1</p>
+          <h1 class="pt-4 pb-10">5</h1>
         </div>
-        <Timer />
+        <div class="player-bg"></div>
       </div>
-    </div>
-    <div class="player-wrapper">
-      <div class="player player-2">
-        <img src="../assets/images/player-two-smiley-face.svg" alt="" />
-        <p class="uppercase">Player 2</p>
-        <h1 class="pt-4 pb-10">5</h1>
+      <div class="board-wrapper">
+        <Board class="board-background" />
+        <svg class="board-overlay">
+          <g v-for="(row, rowIndex) in board" :key="'row-' + rowIndex">
+            <rect
+              v-for="(_, colIndex) in row"
+              :key="rowIndex + '-' + colIndex"
+              :x="colIndex * cellWidth"
+              :y="rowIndex * cellHeight"
+              :width="cellWidth"
+              :height="cellHeight"
+              fill="transparent"
+              :id="rowIndex + '-' + colIndex"
+            />
+          </g>
+        </svg>
+        <div class="timer">
+          <div class="timer-wrapper">
+            <p class="font-bold text-white text-base">PLAYER 1’S TURN</p>
+            <h1 class="text-white">10s</h1>
+          </div>
+          <Timer />
+        </div>
       </div>
-      <div class="player-bg"></div>
+      <div class="player-wrapper">
+        <div class="player player-2">
+          <img src="../assets/images/player-two-smiley-face.svg" alt="" />
+          <p class="uppercase">Player 2</p>
+          <h1 class="pt-4 pb-10">5</h1>
+        </div>
+        <div class="player-bg"></div>
+      </div>
     </div>
   </div>
   <div class="bg-footer"></div>
@@ -67,41 +68,45 @@ const cellHeight = boardHeight / rows;
 @reference "../assets/css/main.css";
 
 .container {
-  @apply mx-auto relative gap-20 z-20 px-10 w-full h-screen flex-row flex items-center justify-center;
+  @apply mx-auto relative z-20 px-10 w-full h-screen flex-col flex items-center justify-center;
 
-  .player-wrapper {
-    @apply relative;
+  .game-board {
+    @apply flex flex-row gap-20 items-center justify-center mx-auto w-full;
 
-    .player {
-      @apply flex flex-col relative z-20 bg-white rounded-4xl border-[3px] border-black pt-10 px-6 text-center;
+    .player-wrapper {
+      @apply relative;
 
-      img {
-        @apply absolute -top-[18%] left-1/2 -translate-x-1/2;
+      .player {
+        @apply flex flex-col relative z-20 bg-white rounded-4xl border-[3px] border-black pt-10 px-6 text-center;
+
+        img {
+          @apply absolute -top-[18%] left-1/2 -translate-x-1/2;
+        }
+      }
+
+      .player-bg {
+        @apply absolute inset-0 bg-black rounded-4xl z-10 translate-y-3;
       }
     }
 
-    .player-bg {
-      @apply absolute inset-0 bg-black rounded-4xl z-10 translate-y-3;
-    }
-  }
+    .board-wrapper {
+      @apply relative w-[630px] h-[590px];
 
-  .board-wrapper {
-    @apply relative w-[630px] h-[590px];
+      .timer {
+        @apply absolute left-1/2 translate-x-[-50%] text-center -bottom-[15%];
 
-    .timer {
-      @apply absolute left-1/2 translate-x-[-50%] text-center -bottom-[20%];
-
-      .timer-wrapper {
-        @apply absolute w-full top-[30%] left-0;
+        .timer-wrapper {
+          @apply absolute w-full top-[30%] left-0;
+        }
       }
-    }
 
-    .board-background {
-      @apply absolute top-0 left-0 w-[632px] h-[596px];
-    }
+      .board-background {
+        @apply absolute top-0 left-0 w-[632px] h-[596px];
+      }
 
-    .board-overlay {
-      @apply absolute top-0 left-0 w-[630px] h-[540px];
+      .board-overlay {
+        @apply absolute top-0 left-0 w-[630px] h-[540px];
+      }
     }
   }
 }
---
+++ components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="header">
+    <button>Menu</button>
+    <img src="../assets/images/logo-connect-four.svg" alt="" />
+  </div>
+</template>
+
+<style scoped>
+@reference "../assets/css/main.css";
+
+.header {
+  @apply absolute top-[5%] 2xl:top-[12%] flex flex-row w-[632px] items-center gap-[12.7rem];
+
+  button {
+    @apply uppercase text-white px-5 py-1.5 bg-dark-blue rounded-4xl cursor-pointer;
+  }
+}
+</style>
