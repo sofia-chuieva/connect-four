c85d3e3aa22cbbb33fd54a53e86d3a8ef3d4cc16
Sofia Chuieva <schuieva@themission.co.uk>
2025-04-23 16:17:57 +0100

    board amends

--- components/GameBoard.vue
+++ components/GameBoard.vue
@@ -5,20 +5,14 @@
       <PlayerCard icon="player-one-smiley-face.svg" :player="1" />
       <div class="board-wrapper">
         <img src="/images/board.svg" alt="Game Board" />
-        <svg class="board-overlay">
-          <g v-for="(row, rowIndex) in board" :key="'row-' + rowIndex">
-            <rect
-              v-for="(_, colIndex) in row"
-              :key="rowIndex + '-' + colIndex"
-              :x="colIndex * cellWidth"
-              :y="rowIndex * cellHeight"
-              :width="cellWidth"
-              :height="cellHeight"
-              fill="transparent"
-              :id="rowIndex + '-' + colIndex"
-            />
-          </g>
-        </svg>
+
+        <div class="cells-grid">
+          <div
+            v-for="cellIndex in cellIndices"
+            :key="cellIndex"
+            :class="['cell', `cell-${cellIndex}`]"
+          ></div>
+        </div>
         <Timer />
       </div>
       <PlayerCard icon="player-two-smiley-face.svg" :player="2" />
@@ -36,10 +30,8 @@ const boardHeight = 420;
 const rows = 6;
 const cols = 7;
 
-const board = ref(Array.from({ length: rows }, () => Array(cols).fill(null)));
-
-const cellWidth = boardWidth / cols;
-const cellHeight = boardHeight / rows;
+const totalCells = rows * cols;
+const cellIndices = Array.from({ length: totalCells }, (_, i) => i);
 </script>
 
 <style scoped>
@@ -54,12 +46,30 @@ const cellHeight = boardHeight / rows;
     .board-wrapper {
       @apply relative w-[500px] h-[420px];
 
-      .board-background {
-        @apply absolute top-0 left-0 w-[632px] h-[596px];
+      .cells-grid {
+        position: absolute;
+        top: 0;
+        display: grid;
+        left: 0;
+        grid-template-columns: repeat(7, 1fr);
+        grid-template-rows: repeat(6, 1fr);
+        gap: 0.8rem;
+        padding: 0.8rem 0.8rem 0;
+        width: 500px;
+        height: 420px;
       }
 
-      .board-overlay {
-        @apply absolute top-0 left-0 w-[500px] h-[420px];
+      .cell {
+        cursor: pointer;
+      }
+
+      .marker {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        pointer-events: none;
       }
     }
   }
